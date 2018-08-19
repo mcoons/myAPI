83112abb9570b989fa9ce018887a61491bcc1eeb
Michael Coons <michael@Michaels-MacBook-Pro-4.local>
2018-08-19 01:13:53 -0600

    heroku addons:create heroku-postgresql

--- queries.js
+++ queries.js
@@ -6,35 +6,35 @@ module.exports = {
     return database("person")
       .select()
       .orderBy("last_name");
-  },
-
-  create(person) {
-    return database("person")
-      .from("person")
-      .insert(person)
-      .returning("*")
-      .then(record => record[0]);
-  },
-
-  read(attribute, value) {
-    return database("person")
-      .select()
-      .where(attribute, value)
-      .orderBy("last_name");
-  },
-
-  update(id, person) {
-    return database("person")
-      .update(person)
-      .where("id", id)
-      .returning("*")
-      .then(record => record[0]);
-  },
-
-  delete(id) {
-    return database("person")
-      .delete()
-      .where("id", id);
   }
+
+//   create(person) {
+//     return database("person")
+//       .from("person")
+//       .insert(person)
+//       .returning("*")
+//       .then(record => record[0]);
+//   },
+
+//   read(attribute, value) {
+//     return database("person")
+//       .select()
+//       .where(attribute, value)
+//       .orderBy("last_name");
+//   },
+
+//   update(id, person) {
+//     return database("person")
+//       .update(person)
+//       .where("id", id)
+//       .returning("*")
+//       .then(record => record[0]);
+//   },
+
+//   delete(id) {
+//     return database("person")
+//       .delete()
+//       .where("id", id);
+//   }
 
 };
