3bb5acfa8abb83ed393ae11829a2af8179e61770
Michael Coons <michael@Michaels-MacBook-Pro-4.local>
2018-08-19 00:17:03 -0600

    added 'queries.js' and 'database-connetion.js' and added  basic 'list person' api route

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // require/import the express module(s)
 const express = require('express'); 
 
+const queries = require("./queries");
+
 // instantiate an instance of the express class as 'app'
 const app = express();              
 
@@ -13,6 +15,54 @@ const port = process.env.PORT || localport;
 
 // create a base route to direct root GET requests to
 app.get('/', (request, response, next) => response.send('Hello There'));      
+
+
+
+//////     CRUD ROUTES     //////
+
+
+app.get("/person", (request, response, next) => {
+    queries
+      .list()
+      .then(person => { response.json({person}); })
+      .catch(next);
+  });
+  
+//   app.post("/person", (request, response) => {
+//     queries
+//       .create(request.body.student)
+//       .then(response.redirect("/"))
+//       .catch(err => { response.send("error: ", err); });
+//   });
+  
+//   app.get("/person/:id", (request, response, next) => {
+//     queries
+//       .read("id",request.params.id)
+//       .then(student => { response.render( "view", { student: student, url:"view" }); })
+//       .catch(next);
+//   });
+  
+//   app.put("/person/:id", (request, response, next) => {
+//     queries
+//       .update(request.params.id, request.body.student)
+//       .then(response.redirect("/person/"+request.params.id))
+//       .catch(next);
+//   });
+  
+//   app.delete("/person/:id", (request, response, next) => {
+//     if (request.params.id === "19"){
+//       response.redirect("/person")
+//     } 
+//     else {
+//       queries
+//         .delete(request.params.id)
+//         .then(response.redirect("/person"))
+//         .catch(next);
+//     }
+//   });
+  
+  
+
 
 // tell the Express app to listen for requests on our port      
 app.listen(port, () => console.log(`Server is now listening on port ${port}`));
---
+++ queries.js
@@ -0,0 +1,40 @@
+const database = require("./database-connection");
+
+module.exports = {
+
+  list() {
+    return database("person")
+      .select()
+      .orderBy("last_name");
+  },
+
+  create(person) {
+    return database("person")
+      .from("person")
+      .insert(person)
+      .returning("*")
+      .then(record => record[0]);
+  },
+
+  read(attribute, value) {
+    return database("person")
+      .select()
+      .where(attribute, value)
+      .orderBy("last_name");
+  },
+
+  update(id, person) {
+    return database("person")
+      .update(person)
+      .where("id", id)
+      .returning("*")
+      .then(record => record[0]);
+  },
+
+  delete(id) {
+    return database("person")
+      .delete()
+      .where("id", id);
+  }
+
+};
